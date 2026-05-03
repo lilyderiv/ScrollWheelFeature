4617858bb2f30d0cf45338981fa6a6a264788c25
lilyderiv <busrakubradereli@gmail.com>
2026-05-03 14:56:30 +0300

    chore: Initial Visual Studio empty project setup

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Scroll_Feature
+{
+    public partial class Form1 : Form
+    {
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           
+        }
+    }
+}
